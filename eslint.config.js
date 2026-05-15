fa50eed69fd7178f2ce48ea9253ef5e185e94005
sai3zung1 <sai3zung1@gmail.com>
2026-05-15 16:14:29 +0200

    chore(SFT-003): update eslint.config.js file with custom rules

--- eslint.config.js
+++ eslint.config.js
@@ -10,42 +10,62 @@ import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig([
   {
+    ignores: [
+      "**/.obsidian/",
+      "**/dist/",
+      "**/node_modules/",
+      "**/.bun-cache/",
+      "**/build/",
+    ],
+  },
+
+  {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    extends: [js.configs.recommended],
+    languageOptions: { 
+      globals: { ...globals.browser, ...globals.node } 
+    },
   },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  ...tseslint.configs.recommended,
+
   {
-    files: ["**/*.json"],
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    ...pluginReact.configs.flat.recommended,
+    rules: {
+      "react/react-in-jsx-scope": "off", // Inutile avec Vite/React 17+
+    },
+  },
+
+  {
+    files: ["**/*.json", "**/*.json5"],
     plugins: { json },
     language: "json/json",
     extends: ["json/recommended"],
   },
   {
-    files: ["**/*.jsonc"],
+    files: ["**/*.jsonc", "**/tsconfig*.json"],
     plugins: { json },
     language: "json/jsonc",
     extends: ["json/recommended"],
   },
-  {
-    files: ["**/*.json5"],
-    plugins: { json },
-    language: "json/json5",
-    extends: ["json/recommended"],
-  },
+
   {
     files: ["**/*.md"],
     plugins: { markdown },
     language: "markdown/gfm",
     extends: ["markdown/recommended"],
+    rules: {
+      "markdown/no-missing-label-refs": "off", // Autorise tes [...] de template
+    }
   },
+
   {
     files: ["**/*.css"],
     plugins: { css },
     language: "css/css",
     extends: ["css/recommended"],
   },
+
   eslintConfigPrettier,
 ]);
